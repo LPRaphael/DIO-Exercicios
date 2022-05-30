4e2d187014dfef5c71cd109af0548bab049ab8c4
Raphael Lyra Perazzo Leite Galvão <raphael.perazzo@gmail.com>
2022-05-30 11:30:20 -0300

    criado uma tabela simples

--- DB-Dio.sql
+++ DB-Dio.sql
@@ -5,9 +5,17 @@ CREATE TABLE `Dio`.`pessoas` (
   `nascimento` DATE
 );
 
+ALTER TABLE `Dio`.`pessoas` 
+ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST,
+CHANGE COLUMN `nome` `nome` VARCHAR(20) NOT NULL ,
+ADD PRIMARY KEY (`id`);
+;
+
 CREATE TABLE `Dio`.`pessoa` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(20) NULL,
   `pessoacol` VARCHAR(45) NULL,
   `nascimento` DATE NULL,
   PRIMARY KEY (`id`));
+
+  INSERT INTO pessoas (nome, nascimento) VALUES ('Raphael', '1989-11-25');
